e2e78bf225efd57bdb6e7d87d9db24638320a7c9
amunt0 <25135388+adrianmuntele@users.noreply.github.com>
2025-02-12 14:18:37 +0200

    change module

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -3,8 +3,3 @@ output "argocd_namespace" {
   value       = var.namespace
 }
 
-output "root_application_name" {
-  description = "Name of the root application"
-  value       = kubernetes_manifest.root_application.manifest.metadata.name
-}
-
